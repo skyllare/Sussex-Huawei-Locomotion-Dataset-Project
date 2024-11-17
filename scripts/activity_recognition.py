c1863669d25065009269610fa83b239c9cb08e13
skyllarautumn <skyllarautumn@gmail.com>
2024-11-16 21:57:13 -0800

    added more columns from the data

--- scripts/activity_recognition.py
+++ scripts/activity_recognition.py
@@ -1,27 +1,35 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+import math
 
-# the memory may need to be upped for the full dataset
-spark = SparkSession.builder \
-    .appName("MongoDBConnection") \
-    .config("spark.mongodb.input.uri", "mongodb://localhost:27017/Project_Test.Label") \
-    .config("spark.mongodb.output.uri", "mongodb://localhost:27017/Project_Test.Label") \
-    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1") \
-    .config("spark.driver.memory", "4g") \
-    .getOrCreate()
+def get_data():
+    # the memory may need to be upped for the full dataset
+    spark = SparkSession.builder \
+        .appName("ActivityRecognition") \
+        .config("spark.mongodb.input.uri", "mongodb://localhost:27017/Project_Test.Label") \
+        .config("spark.mongodb.output.uri", "mongodb://localhost:27017/Project_Test.Label") \
+        .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1") \
+        .config("spark.driver.memory", "4g") \
+        .getOrCreate()
 
-Label_Data = spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
-Label_Data = Label_Data.select("date", "Time", "CoarseLabel")
+    Label_Data = spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
+    Label_Data = Label_Data.select("date", "Time", "CoarseLabel")
 
-Hips_Data = spark.read.format("com.mongodb.spark.sql.DefaultSource") \
-    .option("spark.mongodb.input.uri", "mongodb://localhost:27017/Project_Test.Hips_Motion") \
-    .load()
+    Hips_Data = spark.read.format("com.mongodb.spark.sql.DefaultSource") \
+        .option("spark.mongodb.input.uri", "mongodb://localhost:27017/Project_Test.Hips_Motion") \
+        .load()
 
-# Optionally, select specific columns from the Hips_Motion collection
-Hips_Data = Hips_Data.select("date", "Time", "AccelerationX", "AccelerationY")  # Adjust columns as needed
+    # Optionally, select specific columns from the Hips_Motion collection
+    Hips_Data = Hips_Data.select("date", "Time", 
+                                 "AccelerationX", "AccelerationY", "AccelerationZ",
+                                 "MagnetometerX", "MagnetometerY", "MagnetometerZ",
+                                 "LinearAcceleartionX", "LinearAcceleartion", "LinearAcceleartionZ")  # Adjust columns as needed
 
-# Join the two datasets on the common columns (e.g., "date" and "Time")
-combined_data = Label_Data.join(Hips_Data, on=["date", "Time"], how="inner")
+    # Join the two datasets on the common columns (e.g., "date" and "Time")
+    return Label_Data.join(Hips_Data, on=["date", "Time"], how="inner")
 
+# def build_tree(combined_data):
+    
 
-# Show the combined result
+combined_data = get_data()
 combined_data.show()
