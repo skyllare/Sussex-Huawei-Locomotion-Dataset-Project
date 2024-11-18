eefbc8095a835116c5e2f06442b707103d38ed62
skyllarautumn <skyllarautumn@gmail.com>
2024-11-17 21:34:13 -0800

    Running on full dataset

--- scripts/random_forest.py
+++ scripts/random_forest.py
@@ -6,8 +6,8 @@ from pyspark.ml.classification import DecisionTreeClassifier
 # Spark session initialization
 spark = SparkSession.builder \
     .appName("ActivityRecognition") \
-    .config("spark.mongodb.input.uri", "mongodb://localhost:27017/Project_Test.Label") \
-    .config("spark.mongodb.output.uri", "mongodb://localhost:27017/Project_Test.Label") \
+    .config("spark.mongodb.input.uri", "mongodb://localhost:27017/Project.Label") \
+    .config("spark.mongodb.output.uri", "mongodb://localhost:27017/Project.Label") \
     .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1") \
     .config("spark.driver.memory", "8g") \
     .config("spark.executor.memory", "16g") \
